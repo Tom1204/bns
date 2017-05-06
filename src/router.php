970145fdae506bc18582d8bae07a7443cf1f287b
Tom <isoqov12@gmail.com>
2017-05-06 18:19:26 +0500

    changes

--- src/router.php
+++ src/router.php
@@ -3,6 +3,7 @@ require_once "basic/Url.php";
 require_once "Controllers/Registration.php";
 require_once "Controllers/Login.php";
 require_once "Controllers/Dashboard.php";
+require_once "Controllers/Users.php";
 
 
 class Urls extends Url
@@ -11,5 +12,6 @@ class Urls extends Url
         "/^registration/" => array("Registration", "as_view"),
         "/^login/" => array("Login", "as_view"),
         "/^dashboard/" => array("Dashboard", "as_view"),
+        "/^users/" => array("Users", "as_view"),
     );
 }
